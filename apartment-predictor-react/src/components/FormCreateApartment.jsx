4a41cc107b38a758493f6de64cae5a27523d6b43
nil.sanca97 <nil.sanca97@gmail.com>
2026-02-09 18:04:58 +0100

    listWithForm with createApartment Button, that has a handleClick that setApartments & setName. Work good and render a list of name the new apartment.

--- apartment-predictor-react/src/components/FormCreateApartment.jsx
+++ apartment-predictor-react/src/components/FormCreateApartment.jsx
@@ -1,5 +1,5 @@
 import { useState } from "react";
-
+ 
 export default function FormCreateApartment() {
   const [apartment, setApartment] = useState({
     title: "beautiful apartment",
--- apartment-predictor-react/src/components/ListWithForm.jsx
+++ apartment-predictor-react/src/components/ListWithForm.jsx
@@ -1,14 +1,58 @@
 import HomeListRender from "./HomeListRender";
 import ApartmentDetails from "./ApartmentDetails";
 import FormCreateApartment from "./FormCreateApartment";
+import { useState } from "react";
+import { Apartment } from "@mui/icons-material";
+import { apartments } from "../data/apartments";
+import CustomizedTables from "../components/CustomizedTables"
+
 //import HomeListRenderWithForm from "../components/HomeListRenderWithForm";
 
-export default function ListWithForm() {
+let nextId = 3;
 
+const initialApartments = [
+        {id: 0, name: 'test apartment 0'}, 
+        {id: 1, name: 'Bright flat in Eixample'}, 
+        {id: 2, name: 'Cozy apartment near the beach'}, 
+]
+export default function ListWithForm() {
+    const [name, setName] = useState('example name apartment');
+    const [apartments, setApartments] = useState( 
+        initialApartments
+    );
+
+    function handleClick() {
+            const nextApartments = [
+            // Apartment before the insertion poin: 
+            ...apartments, 
+            // new apartment: 
+            { id: nextId++, name: name } 
+        ];
+        setApartments(nextApartments);
+        setName('');
+    }
+    
     return (
         <>
             <HomeListRender />
             <FormCreateApartment />
+            <h2> Inspiring sculptors: </h2>
+            <input
+                value={name}
+                onChange={e => setName(e.target.value)}
+            />
+            <button onClick={handleClick}>
+                Create
+            </button>
+            <ul>
+                {apartments.map(apartment => (
+                    <li key={apartment.id}>{apartment.name}</li>
+                ))}
+                
+            </ul>
+            <CustomizedTables
+                apartments= {apartments}  
+            /> 
         </>
     );
 }
