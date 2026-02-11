84845a7e70290ae389361dd611097ed6e3c92f16
nil.sanca97 <nil.sanca97@gmail.com>
2026-02-12 00:44:34 +0100

    CreateNewApartment: clean code and delete comments. Now it's correct. But don't render yet because I need to implement ApartmentsList (ancestor component)

--- apartment-predictor-react/src/components/CreateNewApartment.jsx
+++ apartment-predictor-react/src/components/CreateNewApartment.jsx
@@ -2,33 +2,6 @@ import Form from "../ui/Form";
 import CustomizedTables from "../ui/CustomizedTables";
 
 export default function CreateNewApartment({apartmentsList, setApartmentsList, formData, setFormData}) {
-  //const [isRegistered, setIsRegistered] = useState(false);
-  const [apartmentsList, setApartmentsList] = useState(apartments);
-  const [formData, setFormData] = useState({
-    id: "",
-    title: "",
-    price: "",
-    surface:"", 
-    rooms: "", 
-  });
-
-  /*if (isRegistered) {
-    return (
-      <>
-        <h4>
-          Apartment {apartmentsList.id} created sucessfull. Here below you have details of apartment
-          created.{" "}
-        </h4>{" "}
-        <b> TITLE: </b> {formData.title},
-        <b> PRICE: </b> {formData.price}, 
-        <b> SURFACE: </b> {formData.surface}, 
-        <b> ROOMS: </b> {formData.rooms}
-        <CustomizedTables
-          apartments= {apartmentsList}  
-        /> 
-      </>
-    );
-  }*/
 
     function handleChange(e) {
       setFormData({
@@ -51,7 +24,6 @@ export default function CreateNewApartment({apartmentsList, setApartmentsList, f
       newApartment
     ]);
 
-    //setIsRegistered(true);
   }
   return (
     <>
@@ -61,7 +33,7 @@ export default function CreateNewApartment({apartmentsList, setApartmentsList, f
         handleSubmit = {handleSubmit}
       />  
       <CustomizedTables
-        apartmentsList = {apartmentsList}
+        apartments = {apartmentsList}
       />
     </>  
   );
