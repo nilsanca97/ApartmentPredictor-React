bd485248f0845498e91a0679fe36ac00ebbdf663
nil.sanca97 <nil.sanca97@gmail.com>
2026-02-12 04:32:20 +0100

    add analert to CreateNewApartment

--- apartment-predictor-react/src/components/CreateNewApartment.jsx
+++ apartment-predictor-react/src/components/CreateNewApartment.jsx
@@ -35,6 +35,7 @@ export default function CreateNewApartment() {
       ...apartmentsList, 
       newApartment
     ]);
+    alert('Your new apartment is created sucessfully');
 
     //setIsRegistered(true);
   }
