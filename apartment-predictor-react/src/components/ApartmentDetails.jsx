60b8360a41e0d38d1a3442ffad356f5b33182d98
nil.sanca97 <nil.sanca97@gmail.com>
2026-02-12 19:35:44 +0100

    desacoppling component ApartmeentDetails in 2 components: card (paints apartments data that recives by props from APartmentDetails).

--- apartment-predictor-react/src/components/ApartmentDetails.jsx
+++ apartment-predictor-react/src/components/ApartmentDetails.jsx
@@ -1,84 +1,12 @@
-//import { initialApartments } from "../data/apartments";
-/*import Card from '@mui/material/Card';
-import CardContent from '@mui/material/CardContent';
-import Typography from '@mui/material/Typography';
-import CardActionArea from '@mui/material/CardActionArea';
-import AccordionExpandIcon from "../ui/AccordionExpandIcon";
-
-export default function ApartmentDetailsCard({ apartments = []}) {
-    
-    /*const idApartmentToFilter = props.id;
-    const detailsToRender = initialApartments.filter(
-        (apartment) => apartment.id === idApartmentToFilter
-    );
-    
-    console.log("Apartments recibidos:", apartments);
-    const detailsListItems = apartments.map((apartment) => (
-        <Card key= {apartment.id} sx={{ maxWidth: 345 }}>
-            <CardActionArea>
-                <img
-                    src= {apartment.imageUrl} 
-                    alt= {apartment.title}   
-                />
-                <CardContent>
-                    <Typography gutterBottom variant="h5" component="div">
-                        {apartment.title}
-                    </Typography>
-                    <AccordionExpandIcon 
-                        id= {apartment.id}
-                        description={apartment.description}
-                        price={apartment.price}
-                        rooms={apartment.rooms}
-                    />
-                </CardContent>
-            </CardActionArea>
-        </Card>
-    ));
-    
-
-    return (
-        <>
-            {detailsListItems}
-        </>  
-    );
-}*/
-// ApartmentDetails.jsx
-import Card from '@mui/material/Card';
-import CardContent from '@mui/material/CardContent';
-import Typography from '@mui/material/Typography';
-import CardActionArea from '@mui/material/CardActionArea';
-import AccordionExpandIcon from "../ui/AccordionExpandIcon";
-
 export default function ApartmentDetails({ apartments = [] }) {
-  console.log("Apartments recibidos:", apartments);
-
-  const detailsListItems = apartments.map((apartment) => (
-    <Card key={apartment.id} sx={{ maxWidth: 345 }}>
-      <CardActionArea>
-        <img
-          src={apartment.imageUrl}
-          alt={apartment.title}
-        />
-        <CardContent>
-          <Typography gutterBottom variant="h5" component="div">
-            {apartment.title}
-          </Typography>
-          <AccordionExpandIcon
-            id={apartment.id}
-            description={apartment.description}
-            price={apartment.price}
-            rooms={apartment.rooms}
-          />
-        </CardContent>
-      </CardActionArea>
-    </Card>
-  ));
 
   return (
     <>
       <h2>More details about apartments</h2>
       <p>Here you can see more details about all apartments (in card format)</p>
-      {detailsListItems}
+      <CustomizedCard 
+        apartments= {apartments}
+      />
     </>
   );
 }
